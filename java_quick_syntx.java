9608a8b89140d103181170dd1cb7d6e2843f5ce0
Vibhav Srivastava <35532045+vibhav13@users.noreply.github.com>
2020-06-22 16:38:25 +0200

    Adding comment

--- java_quick_syntx.java
+++ java_quick_syntx.java
@@ -9,6 +9,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+	  // can be formatted later  
 	 // Write your code here
 //        System.out.println("Hello World");
      /* Variables
